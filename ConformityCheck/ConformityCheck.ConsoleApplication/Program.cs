17acbea566f0f0cfa92bc65b81b892e592e4df4f
Paulina-Dyulgerska <paylina_st@yahoo.com>
2020-09-22 13:13:00 +0300

    Project Structure Upload

---
+++ ConformityCheck/ConformityCheck.ConsoleApplication/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConformityCheck.ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
