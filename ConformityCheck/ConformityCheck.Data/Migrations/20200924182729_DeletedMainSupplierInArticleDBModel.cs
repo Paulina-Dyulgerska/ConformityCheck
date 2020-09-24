56906ca035f3265f7d2a9b140e3077bc5b4b8b09
Paulina-Dyulgerska <paylina_st@yahoo.com>
2020-09-24 21:28:50 +0300

    Migration add

---
+++ ConformityCheck/ConformityCheck.Data/Migrations/20200924182729_DeletedMainSupplierInArticleDBModel.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ConformityCheck.Data.Migrations
+{
+    public partial class DeletedMainSupplierInArticleDBModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
