94bd3de96282a71ca35e9ba44ea9e147f2858b0d
Paulina-Dyulgerska <paylina_st@yahoo.com>
2020-09-29 20:56:03 +0300

    Add Importer

--- ConformityCheck/ConformityCheck.Importer/Program.cs
+++ ConformityCheck/ConformityCheck.Importer/Program.cs
@@ -1,12 +1,57 @@
-﻿using System;
+﻿using ConformityCheck.Data;
+using ConformityCheck.Services;
+using ConformityCheck.Services.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
 
 namespace ConformityCheck.Importer
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            var db = new ConformityCheckContext();
+
+            //Add articles:
+            IArticleService articleService = new ArticleService(db);
+            var jsonArticles = File.ReadAllText("ArticlesData.json");
+            var articles = JsonSerializer.Deserialize<IEnumerable<ArticleImportDTO>>(jsonArticles);
+
+            foreach (var article in articles)
+            {
+                try
+                {
+                    articleService.Create(article);
+                }
+                catch (Exception)
+                {
+
+                    //throw; //az ne znam kakvo shte pravq s tazi error i prodyljavam natatyk, kato prosto nqma
+                    //da zapisha nishto v DB-a. No moqt Service throwna error i toj se hvana tuk - ot klienta na 
+                    //moq Service, no kojto shte polzwa tozi cod, shte reshi kakwo da pravi s error-a. Az samo mu davam 
+                    //info za towa kakwo ne e nared, towa mu prashta Service na tozi, kojto go polzwa. Towa da pravi Service
+                    //error na polzwashtiqt go, e pravilnoto povedenie na Service-to!!!
+                }
+            }
+            
+            //Add conformity types:
+            IConformityTypeService conformityTypeService = new ConformityTypeService(db);
+            var jsonConformityTypes = File.ReadAllText("ConformityTypesData.json");
+            var conformityTypes = JsonSerializer.Deserialize<IEnumerable<ConformityTypeImputDTO>>(jsonConformityTypes);
+
+            foreach (var conformityType in conformityTypes)
+            {
+                try
+                {
+                    conformityTypeService.Create(conformityType);
+                }
+                catch (Exception)
+                {
+                }
+            }
         }
     }
 }
